91cf4fb161db63bb8b4944cd870db585299fd526
Barklim <46284548+Barklim@users.noreply.github.c>
2024-06-11 21:16:57 +0800

    fix: use local storage

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const PROD_API_URL = 'https://todo-list-json-server-hnyxyyqsa-klim-barks-projects.vercel.app';
 const DEV_API_URL = 'http://localhost:3000';
 
-export const USE_API = true;
+export const USE_API = false;
 
 export const API_URL = process.env.NODE_ENV === 'production' ? PROD_API_URL : DEV_API_URL;
 
